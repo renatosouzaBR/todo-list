5b08854ec5248d7d4f48441fc9406d45ab55e194
Renato Alves Souza <renatoalvessouza@MacBook-Air-de-Renato.local>
2022-10-13 09:06:32 -0300

    chore: add tailwind setup

--- projects/design-system/src/main.tsx
+++ projects/design-system/src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "./global/styles.css";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
