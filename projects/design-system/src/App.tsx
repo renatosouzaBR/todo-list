8b5b280dc653e33f810685e5cee044e166f11c51
Renato Alves Souza <renatoalvessouza@MacBook-Air-de-Renato.local>
2022-10-23 18:54:23 -0300

    chore: add tokens on the global theme

--- projects/design-system/src/App.tsx
+++ projects/design-system/src/App.tsx
@@ -1,3 +1,10 @@
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-gray-900">Hello World</h1>
+      <button className="bg-cyan-500 text-gray-100 rounded px-4 py-2 hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  );
 }
--- projects/design-system/tailwind.config.cjs
+++ projects/design-system/tailwind.config.cjs
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
+    fontSize: {
+      sm: 12,
+      md: 16,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      "gray-900": "#171717",
+      "gray-800": "#1E1E1E",
+      "gray-400": "#52525B",
+      "gray-300": "#A1A1AA",
+      "gray-100": "#F4F4F5",
+
+      "cyan-500": "#61DAFB",
+      "cyan-300": "#69DBFA",
+    },
     extend: {
       fontFamily: {
         sans: "Inter, sans-serif",
