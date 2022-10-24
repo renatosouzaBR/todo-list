568c69f86fb92ad14a56b600417da990935646f8
Renato Alves Souza <renatoalvessouza@MacBook-Air-de-Renato.local>
2022-10-24 14:14:09 -0300

    refactor: extends props in the components

--- projects/design-system/src/components/CheckBox.tsx
+++ projects/design-system/src/components/CheckBox.tsx
@@ -1,9 +1,14 @@
 import { Check } from "phosphor-react";
 import * as RadixCheckbox from "@radix-ui/react-checkbox";
 
-export function CheckBox() {
+interface CheckBoxProps extends RadixCheckbox.CheckboxProps {}
+
+export function CheckBox(props: CheckBoxProps) {
   return (
-    <RadixCheckbox.Root className="flex items-center h-4 w-4 bg-gray-400 rounded">
+    <RadixCheckbox.Root
+      className="flex items-center h-4 w-4 bg-gray-400 rounded"
+      {...props}
+    >
       <RadixCheckbox.Indicator>
         <Check weight="bold" className="h-4 w-4 text-cyan-500" />
       </RadixCheckbox.Indicator>
