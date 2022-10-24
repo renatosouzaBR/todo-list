d69a3f032ede59b08cdf870dfda8d2b36f32a11f
Renato Alves Souza <renatoalvessouza@MacBook-Air-de-Renato.local>
2022-10-23 21:26:13 -0300

    feat: add Text component

---
+++ projects/design-system/src/components/Text.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
+
+export interface TextProps {
+  size?: "sm" | "md" | "lg";
+  children: ReactNode;
+  asChild?: boolean;
+}
+
+export function Text({ size = "md", asChild, children }: TextProps) {
+  const Comp = asChild ? Slot : "span";
+
+  return (
+    <Comp
+      className={clsx("text-gray-100 font-sans", {
+        "text-sm": size === "sm",
+        "text-md": size === "md",
+        "text-lg": size === "lg",
+      })}
+    >
+      {children}
+    </Comp>
+  );
+}
