295739acdff9d4876565a36b9b872d39ae3d4d8f
Renato Souza <natoicm@gmail.com>
2023-03-04 22:54:22 -0300

    feat: add input to add a new task

--- projects/todo/src/App.tsx
+++ projects/todo/src/App.tsx
@@ -1,9 +1,25 @@
 import { Header } from "./components/Header";
 
+import addSvg from './assets/add.svg';
+import styles from './App.module.css';
+
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.todoList}>
+        <div className={styles.newTaskInputGroup}>
+          <input
+            placeholder="Adicione uma nova tarefa"
+          />
+
+          <button>
+            Criar
+            <img src={addSvg} />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
