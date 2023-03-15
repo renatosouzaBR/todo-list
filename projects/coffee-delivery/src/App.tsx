426e1cb0bf288a3e218ba1e8115a38c73eccb128
Renato Souza <natoicm@gmail.com>
2023-03-15 17:28:37 -0300

    chore: add global styles

--- projects/coffee-delivery/src/App.tsx
+++ projects/coffee-delivery/src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
+import { DefaultTheme } from './styles/DefaultTheme'
+import { GlobalStyle } from './styles/GlobalStyle'
+
 export function App() {
-  return <h1>Coffee Delivery</h1>
+  return (
+    <ThemeProvider theme={DefaultTheme}>
+      <GlobalStyle />
+
+      <h1>Coffee Delivery</h1>
+    </ThemeProvider>
+  )
 }
---
+++ projects/coffee-delivery/src/styles/DefaultTheme.ts
@@ -0,0 +1,21 @@
+export const DefaultTheme = {
+  'yellow-900': '#C47F17',
+  'yellow-400': '#DBAC2C',
+  'yellow-100': '#F1E9C9',
+
+  'purple-900': '#4B2995',
+  'purple-400': '#8047F8',
+  'purple-100': '#EBE5F9',
+
+  'base-900': '#272221',
+  'base-800': '#403937',
+  'base-700': '#574F4D',
+  'base-600': '#8D8686',
+  'base-400': '#D7D5D5',
+  'base-300': '#E6E5E5',
+  'base-200': '#EDEDED',
+  'base-100': '#F3F2F2',
+
+  background: '#FAFAFA',
+  white: '#FFFFFF',
+}
